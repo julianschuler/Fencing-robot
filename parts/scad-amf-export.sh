c12c0619a4351f8919dce1d7c4a79f062d217318
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2020-05-23 01:45:16 +0200

    Extend and polish AMF export script

--- parts/scad-amf-export.sh
+++ parts/scad-amf-export.sh
@@ -1,55 +1,115 @@
 #!/bin/bash
 
+
+# defaults
 EXPORT_PATH="AMF"
 EXPORT_EXT="amf"
 
-PARTS="scad-part-names.conf"
-INCLUDE_PATH="$(pwd)/scad"
-
-USAGE="Use \"-a\" to render and export all parts or \"-p <part-name>\" for a specific part."
+PART_NAME_FILE="scad-part-names.conf"
+INCLUDE_PATH="scad"
 
 
+# print correct usage and exit
+print_usage() {
+	echo "Usage: $(basename $BASH_SOURCE) [OPTION]... [part-name]..."
+	echo "Export parts specified by name from OpenSCAD files."
+	echo ""
+	echo "  -a          export all parts using the default settings"
+	echo "  -f <file>   file to read part from"
+	echo "  -i <file>   .scad file to include"
+	echo "  -I <dir>    path to directory for .scad files to include"
+	echo "  -o <dir>    path to directory for rendered files to be exported"
+	echo "  -t <ext>    export file type (default: amf)"
+	echo "  -h          open this help chart"
+	exit 0
+}
+
+
+# print error message and exit
+print_error() {
+	if [[ "$#" > 0 ]]; then
+		echo "$1" 1>&2
+	fi
+	echo "Use \"$(basename $BASH_SOURCE) -h\" for further information." 1>&2
+	exit 1
+}
+
+
+# render and export part with the name given by the first parameter
 render_and_export() {
 	local start=$SECONDS
 	echo "Rendering part $1..."
-	if openscad -q -o $EXPORT_PATH/$1.$EXPORT_EXT <(echo "$INCLUDES $1();"); then
+	if openscad -q -o $export_path/$1.$export_ext <(echo "$includes $1();"); then
 		local d=$(($SECONDS - $start))
 		echo "Finished in $(($d/3600)) hours, $((($d/60) % 60)) minutes, $(($d % 60)) seconds."
-		echo "Part exported to $EXPORT_PATH/$1.$EXPORT_EXT."
+		echo "Part exported to $export_path/$1.$export_ext."
 	else
 		echo "Error: Failed to render part $1."
 	fi
 }
 
 
-if [[ $@ < 1 ]]; then 
-	echo "Error: No arguments specified."
-	echo "$USAGE"
-	exit 1
+
+declare -a parts
+file=""
+includes=""
+include_path="$(readlink -f $INCLUDE_PATH)"
+export_path="$EXPORT_PATH"
+export_ext="$EXPORT_EXT"
+
+
+# check if arguments were specified
+if [[ "$#" == 0 ]]; then
+	print_error "No arguments specified."
 fi
 
-INCLUDES=""
-for f in "$INCLUDE_PATH"/*.scad; do
-	INCLUDES+="use<$f>"
+# parse flags
+while getopts 'af:i:I:o:e:h' flag; do
+	case "${flag}" in
+		a) file="$PART_NAME_FILE" ;;
+		f) file="${OPTARG}" ;;
+		i) includes="use<${OPTARG}>" ;;
+		I) include_path="$(readlink -f ${OPTARG})" ;;
+		o) export_path="${OPTARG}" ;;
+		t) export_ext="${OPTARG}" ;;
+		h) print_usage ;;
+		?) print_error ;;
+	esac
+done
+shift $(( OPTIND - 1 ))
+
+# include remaining argments as part names
+for part in "$@"; do
+	parts+=("$part")
 done
 
-SECONDS=0
-if [[ $1 == "-a" ]]; then
+# include files at the include path
+for f in "$include_path"/*.scad; do
+	includes+="use<$f>"
+done
+
+# read part names from file if specified
+if [[ -n "$file" ]]; then
+	if [[ ! -f "$file" ]]; then
+		print_error "File \"$file\" not found."
+	fi
 	while read -r line; do
-		if [[ -n $line ]] && [[ ! ${line:0:1} == "#" ]]; then
-			render_and_export $line
+		if [[ -n "$line" ]] && [[ ! "${line:0:1}" == "#" ]]; then
+			parts+=("$line")
 		fi
-	done < $PARTS
-elif [[ $1 == "-p" ]]; then
-	render_and_export $2
-elif [[ $1 == "-h" ]]; then
-	echo "$USAGE"
-	exit 0
-else
-	echo "Error: Invalid argument."
-	echo $USAGE
-	exit 1
+	done < "$file"
 fi
+
+if [[ "${#parts[*]}" == 0 ]]; then
+	print_error "No parts specified."
+fi
+
+# render specified parts
+SECONDS=0
+for part in "${parts[@]}"; do
+	render_and_export "$part" "$includes"
+done
 d=$SECONDS
 
+# print out total rendering and export time
 echo "Total rendering and export time: $(($d / 3600)) hours, $((($d / 60) % 60)) minutes, $(($d % 60)) seconds."
